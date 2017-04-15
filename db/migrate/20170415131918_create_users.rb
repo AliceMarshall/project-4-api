7ae6d788c3f8bf8ae01f75ddbca071514ae24102
AliceMarshall <marshall.alice92@gmail.com>
2017-04-15 14:21:13 +0100

    User, Items, Categories created

---
+++ db/migrate/20170415131918_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :full_name
+      t.string :email
+      t.integer :telephone
+      t.string :profile_image
+
+      t.timestamps
+    end
+  end
+end
